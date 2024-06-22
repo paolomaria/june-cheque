3fb66309bab08adb46d29332121f4e75ae482059
Paolo Ganci <paolo.ganci@nevis-security.com>
2024-06-22 14:23:57 +0200

    First try to generate a debi package

---
+++ Makefile
@@ -0,0 +1,33 @@
+include config/config.proj
+
+SED_RULES= \
+	-e "s@^MY_PATH=.*@MY_PATH=/opt/june-cheque/bin@g" \
+	-e "s@__VERSION__@${VERSION}@g"
+
+PACKAGE_NAME=june-cheque.${VERSION}
+
+pkg-debi: spool/pkg/${PACKAGE_NAME}/DEBIAN/control spool/pkg/${PACKAGE_NAME}/DEBIAN/postinst spool/pkg/${PACKAGE_NAME}/DEBIAN/postrm
+	if [ -f ${PACKAGE_NAME}.deb ]; then \
+		echo "There is already a package file ${PACKAGE_NAME}.deb"; \
+		exit 1; \
+	fi
+	mkdir -p spool/pkg/${PACKAGE_NAME}/opt/june-cheque/bin
+	cp *.py *.sh spool/pkg/${PACKAGE_NAME}/opt/june-cheque/bin
+	cat createCheques.sh  | sed ${SED_RULES} > spool/pkg/${PACKAGE_NAME}/opt/june-cheque/bin/createCheques.sh
+	mkdir -p spool/pkg/${PACKAGE_NAME}/opt/june-cheque/images
+	cp images/logo.png spool/pkg/${PACKAGE_NAME}/opt/june-cheque/images
+	chmod 755 spool/pkg/${PACKAGE_NAME}/DEBIAN/postinst spool/pkg/${PACKAGE_NAME}/DEBIAN/postrm
+	cd spool/pkg; dpkg-deb -Z xz --build ${PACKAGE_NAME}
+	mv spool/pkg/${PACKAGE_NAME}.deb .
+	${MAKE} clean-pkg
+	echo "The package ${PACKAGE_NAME}.deb is ready"
+	
+	
+clean: clean-pkg
+
+clean-pkg:
+	rm -rf spool/pkg
+
+spool/pkg/${PACKAGE_NAME}/DEBIAN/%: pkg/debian/%.template
+	mkdir -p spool/pkg/${PACKAGE_NAME}/DEBIAN
+	cat $^ | sed ${SED_RULES} > $@
--- createCheques.sh
+++ createCheques.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+MY_PATH=.
+
 function show_dep_text {
 	echo 'Checking presence of the required shell programs (openssl, python3, srm, silkaj)...'
 }
@@ -41,7 +43,7 @@ weblink=$JUNE_CHEQUE_WEBLINK
 now=`date +"%d/%m/%Y"`
 
 function show_usage {
-  echo "Usage: createCheques.sh -n <number of cheques> -a <amount of each cheques> [-s] -o <output file> [-c <link to website running cesium or similar>]"
+  echo "Usage: $0 -n <number of cheques> -a <amount of each cheques> [-s] -o <output file> [-c <link to website running cesium or similar>]"
   echo "    -s: simulate only. Don't tranfer any money."
   echo "    -c: default is '$JUNE_CHEQUE_WEBLINK' (env variable JUNE_CHEQUE_WEBLINK)."
 }
@@ -95,7 +97,7 @@ if [ $number -gt 10 ]; then
 	exit 1
 fi
 
-if [ -f $outputFile ]; then
+if [ -s $outputFile ]; then
 	echo "The file $outputFile already exists"
 	exit 1
 fi
@@ -111,7 +113,7 @@ read -sp 'Password: ' secretPw
 echo
 
 owners_lookup_failed=0
-owners_pubkey=`python3 create_public_key.py "$secretId" "$secretPw"`
+owners_pubkey=`python3 ${MY_PATH}/create_public_key.py "$secretId" "$secretPw"`
 owners_lookup=`silkaj  wot lookup $owners_pubkey` || owners_lookup_failed=1
 
 #echo "$owners_lookup"
@@ -125,7 +127,7 @@ else
 	noProfileFound=0
 	pubKeyFile=$(mktemp  /tmp/.XXXXXXXXX)
 	chmod 600 $pubKeyFile
-	python3 save_and_load_private_key_file_pubsec.py  $secretId $secretPw $pubKeyFile
+	python3 ${MY_PATH}/save_and_load_private_key_file_pubsec.py  $secretId $secretPw $pubKeyFile
 	profile=`jaklis -k $pubKeyFile  get` || noProfileFound=1
 	srm $pubKeyFile
 	if [ $noProfileFound -ne 0 ]; then
@@ -178,7 +180,7 @@ for (( i = 0 ; $i < $number; i = $i + 1)) ; do
 	
 	ctr=$(($ctr + 1))
 	identifiant="${name}-$ctr"
-	pubkey=`python3 create_public_key.py "${identifiant}" "$passFormatted"`
+	pubkey=`python3 ${MY_PATH}/create_public_key.py "${identifiant}" "$passFormatted"`
 	echo "Émis par $ownersPseudo ($owners_pubkey) le $now" >> $outputFile
 	echo "Pour: ___________________________, le __/__/____" >> $outputFile
 	echo "  Identifiant secret: ${identifiant}" >> $outputFile
@@ -189,14 +191,14 @@ for (( i = 0 ; $i < $number; i = $i + 1)) ; do
 		if [ $hasJaklis -eq 1 ]; then
 			pubKeyFile=$(mktemp  /tmp/.XXXXXXXXX)
 			chmod 600 $pubKeyFile
-			python3 save_and_load_private_key_file_pubsec.py  ${identifiant} $passFormatted $pubKeyFile
-			jaklis -k $pubKeyFile  set -d "Chèque June émis le $now" -A logo.png
+			python3 ${MY_PATH}/save_and_load_private_key_file_pubsec.py  ${identifiant} $passFormatted $pubKeyFile
+			jaklis -k $pubKeyFile  set -d "Chèque June émis le $now" -A ${MY_PATH}/images/logo.png
 			srm $pubKeyFile
 		fi
 
 		tfile=$(mktemp  /tmp/.XXXXXXXXX)
 		chmod 600 $tfile
-		python3 createKeyFile.py "$secretId" "$secretPw" "$tfile"
+		python3 ${MY_PATH}/createKeyFile.py "$secretId" "$secretPw" "$tfile"
 		silkaj -af --file "$tfile" money transfer -a $amount -r "$pubkey" -c "cheque  $pubkey" -y
 		srm $tfile
 		echo "  Valeur: $amount June." >> $outputFile
